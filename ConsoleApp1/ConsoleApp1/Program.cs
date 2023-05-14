bc40255ae68d038273d682d95022dee556ef5355
Kari Igiński <charlinski@outlook.com>
2023-05-14 06:53:03 +0200

    joke

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Programmer programmer = new Programmer();
+            programmer.QuitJob();
+        }
+    }
+
+    class Programmer
+    {
+        bool understandsArrays = false;
+
+        public void QuitJob()
+        {
+            if (!understandsArrays)
+            {
+                Console.WriteLine("Why did the programmer quit his job? Because he didn't get arrays - he couldn't decide if the arrays in his code or the array of choices in his life were better!");
+                // Programmer quits job
+            }
+        }
+    }
+}
